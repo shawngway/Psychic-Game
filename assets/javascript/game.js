7926e2192ac2eb2f4fe882940332b738b3c1512f
ShawnG <shawngway@gmail.com>
2019-07-30 19:38:05 -0400

    conditionals partial set, computer bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,32 +7,46 @@ var lossesScore = 0;
 var guessesLeft = 9;
 var guessesMade = [];
 
+
 if (guessesLeft === 0 || guessesLeft === 9) {
 var computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 console.log("computers choice " +computerChoice);
 }
 
+var winsScoreHTML = document.getElementById("winsScore");
+var lossesScoreHTML = document.getElementById("lossesScore");
+var guessesLeftHTML = document.getElementById("guessesLeft");
+var yourChoiceHTML = document.getElementById("guessesMade");
+
+
 document.onkeyup = function(event) { 
-    var winsScoreHTML = document.getElementById("winsScore");
-    // var lossesScoreHTML = docuemnt.getElementById("lossesScore");
-    var guessesLeftHTML = document.getElementById("guessesLeft");
-    var yourChoiceHTML = document.getElementById("guessesMade");
 
     var userChoice = event.key;
-    console.log("users choice " +userChoice);
+    console.log("users choice " + userChoice);
 
 
 
     yourChoiceHTML.innerHTML = " " + userChoice;
 
+
     if (userChoice === computerChoice) {
-        winsScoreHTML.innerHTML = " " + winScore++;
+        winsScore++;
+        winsScoreHTML.innerHTML = " " + winsScore++;
+        alert("you win!");
+        guessesLeft = 9;
+    } else if (userChoice != computerChoice && guessesLeft > 0) {
+        guessesLeftHTML.innerHTML = " " + guessesLeft--;
+
+
     } else {
-    guessesLeftHTML.innerHTML = " " + guessesLeft--;
+        lossesScoreHTML.innerHTML = " " + lossesScore++;
+        guessesLeft = 9;
+        
+
+        
     }
-    // if (guessesLeft = 0) {
-    //     lossesScoreHTML.innerHTML = " " + lossesScore++;
-    // }
+
+    
 
 }
