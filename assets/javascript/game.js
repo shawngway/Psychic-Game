80fb138e7d14bfab5e4cb10c24814acde978cf75
ShawnG <shawngway@gmail.com>
2019-07-30 10:14:11 -0400

    onkeyup set and guesses left set

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,16 +7,32 @@ var lossesScore = 0;
 var guessesLeft = 9;
 var guessesMade = [];
 
+if (guessesLeft === 0 || guessesLeft === 9) {
+var computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+console.log("computers choice " +computerChoice);
+}
+
 document.onkeyup = function(event) { 
     var winsScoreHTML = document.getElementById("winsScore");
-    var lossesScoreHTML = docuemnt.getElementById("lossesScore");
+    // var lossesScoreHTML = docuemnt.getElementById("lossesScore");
     var guessesLeftHTML = document.getElementById("guessesLeft");
     var yourChoiceHTML = document.getElementById("guessesMade");
 
     var userChoice = event.key;
+    console.log("users choice " +userChoice);
 
-    var index = Math.floor(Math.random() * computerChoices.length);
 
-    yourChoiceHTML.innerHTML = " " + unserChoice;
+
+    yourChoiceHTML.innerHTML = " " + userChoice;
+
+    if (userChoice === computerChoice) {
+        winsScoreHTML.innerHTML = " " + winScore++;
+    } else {
+    guessesLeftHTML.innerHTML = " " + guessesLeft--;
+    }
+    // if (guessesLeft = 0) {
+    //     lossesScoreHTML.innerHTML = " " + lossesScore++;
+    // }
 
 }
